1a92b42dd421dd916a23128efa2ad39a590e2449
vivianlu63 <vivianlu63@berkeley.edu>
2020-05-06 19:02:27 -0700

    started training

--- agent.py
+++ agent.py
@@ -4,8 +4,8 @@ from keras.layers import Dense
 from keras.model import Sequential, save_model, load_model
 
 class Agent:
-    
-    def __init__(self, state, discount=0.9, epsilon=.99, nuerons=[32,32,32], 
+
+    def __init__(self, state, discount=0.9, epsilon=.99, nuerons=[32,32,32],
         activation_functions=['relu', 'relu', 'relu', 'relu']):
         self.state = state
         self.discount = discount
@@ -16,7 +16,7 @@ class Agent:
         self.samples = []
 
         self.model = self.build()
-    
+
     def build(self):
         model = Sequential()
         model.add(Dense(self.neurons[0], input_dim=self.state, activation=self.activations_functions[0]))
@@ -24,5 +24,23 @@ class Agent:
             model.add(Dense(self.nuerons[i], activation=self.activations_functions[i]))
         model.add(1, activation=self.activations_functions[self.layers+1])
         model.compile(loss='mse', optimizer='adam')
-        
-        return model        
+
+        return model
+
+    def new_sample(self, state, action, reward, next_state):
+        self.samples.append([state, action, reward, next_state])
+
+    def iterate_once(self, batch_size):
+        index = 0
+        while index < len(self.samples):
+            x = self.samples[index:index + batch_size]
+            yield x
+            index += batch_size
+
+    def train(self, batch_size, epochs):
+        x = []
+        y = []
+
+
+        qVal =
+        self.model.fit(x, y, batch_size = batch_size, epochs = epochs)
